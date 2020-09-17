2df831b285593196fb0ebefa6f983e7f85510e54
Bogdan <>
2020-09-17 06:56:32 +0300

    Первую часть сделали, Ландовский не докопался

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -9,19 +9,23 @@ struct Node                             //Структура, являющаяс
     Node* Next, * Prev;                 //Указатели на адреса следующего и предыдущего элементов списка
 };
 
-class List                              //Создаем тип данных Список
+class Deque                              //Создаем тип данных Список
 {
     Node* Head, * Tail;                 //Указатели на адреса начала списка и его конца
 public:
-    List() :Head(NULL), Tail(NULL) {};    //Инициализируем адреса как пустые
-    ~List();                           //Прототип деструктора
+    Deque() :Head(NULL), Tail(NULL) {};    //Инициализируем адреса как пустые
+    ~Deque();                           //Прототип деструктора
     void show();                       //Прототип функции отображения списка на экране
     void pushBack(float x);                 //Прототип функции добавления элементов в список
     float popBack();
     int size();
+    void pushFront(float x);
+    float popFront();
+    void clear();
+    bool isEmpty();
 };
 
-List::~List()                           //Деструктор
+Deque::~Deque()                           //Деструктор
 {
     while (Head)                       //Пока по адресу на начало списка что-то есть
     {
@@ -31,7 +35,7 @@ List::~List()                           //Деструктор
     }
 }
 
-void List::pushBack(float x)
+void Deque::pushBack(float x)
 {
     Node* temp = new Node;               //Выделение памяти под новый элемент структуры
     temp->Next = NULL;                   //Указываем, что изначально по следующему адресу пусто
@@ -50,17 +54,22 @@ void List::pushBack(float x)
     }
 }
 
-float List::popBack()
+float Deque::popBack()
 {
-    float popValue = Tail->x;
-    Node* newTail = Tail->Prev;
-    delete Tail;
-    Tail = newTail;
-    // сделать уменьшение размера
-    return popValue;
+    if (this->size() != 0)
+    {
+        float popValue = Tail->x;
+        Node* newTail = Tail->Prev;
+        delete Tail;
+        Tail = newTail;
+        Tail->Next = NULL;
+        return popValue;
+    }
+    else
+        throw out_of_range("Nothing to pop!");
 }
 
-int List::size()
+int Deque::size()
 {
     int i = 0;
     Node* temp = Head;                       //Временно указываем на адрес первого элемента
@@ -72,7 +81,66 @@ int List::size()
     return i;
 }
 
-void List::show()
+void Deque::pushFront(float x)
+{
+
+    Node* temp = new Node;               //Выделение памяти под новый элемент структуры
+    temp->Next = Head;                   //Указываем, что изначально по следующему адресу пусто
+    temp->x = x;                         //Записываем значение в структуру
+
+    if (Head != NULL)                    //Если список не пуст
+    {
+        temp->Prev = NULL;               //Указываем адрес на предыдущий элемент в соотв. поле
+        temp->Next = Head;               //Указываем адрес следующего за хвостом элемента
+        Head = temp;                     //Меняем адрес хвоста
+    }
+    else //Если список пустой
+    {
+        temp->Prev = NULL;               //Предыдущий элемент указывает в пустоту
+        Head = Tail = temp;              //Голова=Хвост=тот элемент, что сейчас добавили
+    }
+}
+
+float Deque::popFront()
+{
+    if (this->size() != 0)
+    {
+        float popValue = Head->x;
+        if (this->size() == 1)
+        {
+            delete Head;
+            return popValue;
+        }
+        else
+        {
+            Node* newHead = Head->Next;
+            delete Head;
+            Head = newHead;
+            Head->Prev = NULL;
+            return popValue;
+        }
+    }
+    
+    else
+        throw out_of_range("Nothing to pop!");
+}
+
+void Deque::clear()
+{
+    while (this->size() != 1)              //Пока не встретим пустое значение
+    {
+        this->popFront();
+    }
+    Head = NULL;
+    Tail = NULL;
+}
+
+bool Deque::isEmpty()
+{
+    return this->size() == 0;
+}
+
+void Deque::show()
 {
     /*
     //ВЫВОДИМ СПИСОК С КОНЦА
@@ -98,15 +166,16 @@ void List::show()
 int main()
 {
     system("CLS");
-    List lst; //Объявляем переменную, тип которой есть список
+    Deque lst; //Объявляем переменную, тип которой есть список
     lst.pushBack(100); //Добавляем в список элементы
     lst.pushBack(200);
     lst.pushBack(900);
     lst.pushBack(888);
     lst.show();
-   // cout << lst.popBack() << endl;
-    cout << lst.size() << endl;
+    cout << "Size: " << lst.size() << endl;
+    lst.clear();
     lst.show();//Отображаем список на экране
+    cout << "Size: " << lst.size() << endl;
     system("PAUSE");
     return 0;
 }
